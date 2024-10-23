d8109cfe07d0352823c06eab472cb908cbd5b91d
Varga Marcell <128537619+marci1175@users.noreply.github.com>
2024-10-23 14:48:59 +0200

    cargo fmt cargo clippy added todos

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,15 @@
-use std::sync::Arc;
-
 use drawing_board_client::Application;
 use eframe::NativeOptions;
-use egui::{ColorImage, ImageData, TextureOptions};
 
 fn main() -> eframe::Result<()> {
+    /* TODO:
+        Add textures for painting
+        Create a voice call library
+        Start writing server
+        Create the ability to save the boards
+        Create the ability to have more boards at once
+    */
+    
     let native_options = NativeOptions {
         ..Default::default()
     };
